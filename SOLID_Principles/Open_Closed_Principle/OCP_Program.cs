9c9a33aaf2e2145dbe8734f7146c040ba4f025a4
Riz Kler <rizwan.kler@gmail.com>
2019-03-21 17:15:17 +0000

    SOLID Principles for Software Engineering

---
+++ SOLID_Principles/Open_Closed_Principle/OCP_Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Principle
+{
+    class OCP_Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SOLID_Principles/SOLID_Principles/SRP_Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility_Principle
+{
+    class SRP_Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
